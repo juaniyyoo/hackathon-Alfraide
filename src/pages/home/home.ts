b26fc13e1351f595c78ae3113f835f8ba3f8133a
Juaniyyoo <john.guignard@gmail.com>
2017-07-01 00:32:27 +0200

    Add sample pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DashboardPage } from '../dashboard/dashboard';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  entryComponents:[ DashboardPage ]
 })
 export class HomePage {
+  dashboardPage = DashboardPage;
 
   constructor(public navCtrl: NavController) {
 
