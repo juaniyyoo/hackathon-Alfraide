7412d1e6eca795fa85c37a4f3c36dbff786fa7f8
Kevin Jolbert <Nobodief@MacBook-Pro-de-Kevin.local>
2017-07-01 03:59:38 +0200

    Ajout de contenu dans le settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 /**
  * Generated class for the SettingsPage page.
@@ -14,11 +16,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alerCtrl: AlertController) {
+  }
+  
   users: string = "compte";
   isAndroid: boolean = false;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  
+  doConfirm() {
+    let confirm = this.alerCtrl.create({
+      title: 'Voulez-vous vraiment supprimer votre profil ?',
+      message: 'Votre profil sera supprimé et les données que vous avez enregistrées également, il ne sera plus possible de récupérer votre profil après cette action.',
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: () => {
+            console.log('Action annulé');
+          }
+        },
+        {
+          text: 'Supprimer',
+          handler: () => {
+            console.log('Compte utilisateur supprimé');
+          }
+        }
+      ]
+    });
+    confirm.present()
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
